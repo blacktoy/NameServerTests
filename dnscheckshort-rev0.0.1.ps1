0330b8b89fa3475fda879eb47e9150e5ae35d812
blacktoy <agk4.ri@gmail.com>
2018-12-18 20:16:12 +0700

    first revision

---
+++ dnscheckshort-rev0.0.1.ps1
@@ -0,0 +1,17 @@
+﻿[string[]]$nsvals="1.1.1.1","1.0.0.1","8.8.8.8","8.8.4.4","180.250.13.42","180.250.13.46"
+$Domaincheck="telkom.net"
+#the user isp domain are telkom.net, testing that instead?
+$outputfile="C:\Test1\process.txt"
+#Write-Debug "Directory - $($outputfile) - not found."
+$dirout="C:\Test1"
+if (!(test-path $dirout)) {
+    #create directory if not exist
+    New-Item -ItemType Directory -Force -Path $dirout
+    Write-Host "Directory -$($dirout) - created"
+}
+foreach ($element in $nsvals) {
+    measure-command{nslookup $Domaincheck 2>$null $element}|select TotalSeconds | Out-File -filepath $outputfile -Append
+    <# added 2>$null to avoid output display error 
+        https://superuser.com/questions/1253297/nslookup-in-powershell-always-throws-remoteexception-error
+        #>
+}
